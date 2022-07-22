285f6a5955340e3fa203451ee4eee74aa5375e16
anadzebniauri <anadzebniauri13@gmail.com>
2022-07-22 19:39:10 +0400

    DONE

--- Lecture15.AnaDzebniauri.TableView/DetailsPage.swift
+++ Lecture15.AnaDzebniauri.TableView/DetailsPage.swift
@@ -8,11 +8,54 @@
 import Foundation
 import UIKit
 
-class DetailsPage: UIViewController{
+class DetailsPage: UIViewController {
     
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var lblIMDB: UILabel!
+    @IBOutlet weak var mainActor: UILabel!
+    @IBOutlet weak var isFavorite: UIImageView!
+    
+    var movie : Movie? {
+        didSet {
+            print(movie)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        lblTitle.text = movie?.title
+        releaseDate.text = movie?.releaseDate
+        lblIMDB.text = String(movie!.imdb)
+        mainActor.text = movie?.mainActor
+        
+        
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped))
+        isFavorite.addGestureRecognizer(tapGR)
+        isFavorite.isUserInteractionEnabled = true
+    }
+    
+    @objc func imageTapped(sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            movie!.isFavourite = !movie!.isFavourite
+            
+            let image = UIImage(systemName: movie!.isFavourite ? "star.fill" : "star")
+
+            (sender.view as! UIImageView?)!.image = image
+        }
+    }
+}
+
+extension DetailsPage : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
     }
     
 }
+
--- Lecture15.AnaDzebniauri.TableView/ViewController.swift
+++ Lecture15.AnaDzebniauri.TableView/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController, MovieCellDelegate {
         // Do any additional setup after loading the view.
         movieList.delegate = self
         movieList.dataSource = self
+        
     }
     
     var movies: [Movie] = [Movie(seen: true, title: "Gone Girl", releaseDate: "October 3, 2014", imdb: 8.1, mainActor: "Ben Affleck", isFavourite: true), Movie(seen: false, title: "The Game", releaseDate: "September 12, 1997", imdb: 7.7, mainActor: "Michael Douglas", isFavourite: false), Movie(seen: true, title: "The Social Network", releaseDate: "October 27, 2010", imdb: 7.8, mainActor: "Jesse Eisenberg", isFavourite: true), Movie(seen: true, title: "Se7en", releaseDate: "September 22, 1995", imdb: 8.6, mainActor: "Morgan Freeman", isFavourite: false), Movie(seen: true, title: "Fight Club", releaseDate: "November 11, 1999", imdb: 8.8, mainActor: "Brad Pitt", isFavourite: false), Movie(seen: false, title: "The Girl with the Dragon Tattoo", releaseDate: "December 20, 2011", imdb: 7.8, mainActor: "Daniel Craig", isFavourite: false), Movie(seen: false, title: "Panic Room", releaseDate: "March 18, 2002", imdb: 6.8, mainActor: "Jodie Foster", isFavourite: false), Movie(seen: false, title: "Mank", releaseDate: "November 13, 2020", imdb: 6.8, mainActor: "Gary Oldman", isFavourite: true), Movie(seen: false, title: "Alien3", releaseDate: "May 22, 1992 ", imdb: 6.4, mainActor: "Sigourney Weaver", isFavourite: false), Movie(seen: true, title: "The Girl in the Spider's Web", releaseDate: "October 26, 201", imdb: 6.1, mainActor: "Claire Foy", isFavourite: true)]
@@ -48,11 +49,11 @@ class ViewController: UIViewController, MovieCellDelegate {
 
 struct Movie {
     var seen: Bool
-    let title: String
-    let releaseDate: String
-    let imdb: Double
-    let mainActor: String
-    let isFavourite: Bool
+    var title: String
+    var releaseDate: String
+    var imdb: Double
+    var mainActor: String
+    var isFavourite: Bool
     let description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
 }
 
@@ -110,5 +111,23 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource {
         return cell
     }
         
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("AD - horeeeeyy")
+        // TODO: show next view (implement here for cell select action)
+
+        guard let detailsPage = navigationController?.storyboard?.instantiateViewController(withIdentifier: "DetailsPage") as? DetailsPage else {
+            fatalError("Cant instantiate DetailsPage")
+        }
+        
+        let cell = tableView.cellForRow(at: indexPath) as! MovieCell
+        
+        for i in movies.indices {
+            if movies[i].title == cell.title.text {
+                detailsPage.movie = movies[i]
+            }
+        }
+        
+        navigationController?.pushViewController(detailsPage, animated: true)
+    }
 }
 
